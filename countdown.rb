19da8173aa48ee10fad5f803b9cb3dbb3a4750aa
Noelle Rolwes <nrolwes@gmail.com>
2018-09-11 01:35:15 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,16 +3,16 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1 
   end 
-  "HAPPY NEW YEAR"
+  return "HAPPY NEW YEAR"
 end
 
-countdown_with_sleep(number)
+def countdown_with_sleep(number)
   while number>0 
     puts "#{number} SECOND(S)!"
     sleep(1)
     number -= 1
   end
-  "HAPPY NEW YEAR"
+  return "HAPPY NEW YEAR"
 end
 
 
